d96819c793091bf21f9f5a4b5a2a4b516d2a9214
FranSoto7 <franciscomanuel.sotoramirez@gmail.com>
2021-02-10 14:47:36 +0100

    Added script to populate blockchain with initial offers

---
+++ src/script/data.js
@@ -0,0 +1,40 @@
+module.exports = {
+  endpointAPI: "http://192.168.1.69/data/xAPI",
+
+  Store1: {
+    numberStatements: 261,
+    description:
+      "261 Statements | March-May 2020 | Nationwide Mutual Insurance Company, Advanced Micro Devices, Deree, UGI Corporation, eBay, Whole Foods Market, Susser Holdings Corporation, EOG Resources, Advance Auto Parts, Applied Materials",
+    authorizationHeader:
+      "Basic NjM4NmM5NDhhYjZiMjEwNjlkNzE1YmZmZGNhMWYzZDRhN2FiZWQ3ZTo1ZTkxNDFjYTRiMTdmMDdmYjhiZDA4YTIyOWYwMWJlNWQ3NmZkNGNj",
+    endpointDashboard:
+      "http://192.168.1.69/dashboards/60224e644952370431965714/6023b1067691600647c3b16e/Shareable",
+  },
+  Store2: {
+    numberStatements: 276,
+    description:
+      "276 Statements | March-April 2020 | UGI Corporation, Ross Stores, Applied Materials, Murphy Oil, Nationwide Mutual Insurance Company, eBay, Expeditors International, Whole Foods Market, Advanced Micro Devices, Susser Holdings Corporation",
+    authorizationHeader:
+      "Basic NjBjMTZlMDkxNjAyZTEyOWUwYjUyZTMxNzczYTI5Y2U3MzI5NmU1NDo4YzcwOGNiOTE2ODgxNzY2MDNjYzM1NThmNTZiOTIyOTVkOGZlZWFm",
+    endpointDashboard:
+      "http://192.168.1.69/dashboards/6023b1b47691600647c3b185/6023b2b87691600647c3b1b6/Shareable",
+  },
+  Store3: {
+    numberStatements: 265,
+    description:
+      "265 Statements | April-April 2020 | Deree, C. H. Robinson Worldwide, ConocoPhillips, Anixter, Advanced Micro Devices Assurant, Starwood Hotels and Resorts Worldwide, Computer Sciences Corporation, Ameriprise Financial, Wells Fargo",
+    authorizationHeader:
+      "Basic NWI5ZWZjNDE5YjAzOTYzNTcwMGJiZjI1NzI4ZTRmYWE3MzY0OTRkNjowMTE4YWVkNzhhMGZmNGFhY2Y0NTlkMDI4MTFkYzE3MTA5MThkNGI0",
+    endpointDashboard:
+      "http://192.168.1.69/dashboards/6023b36e7691600647c3b1f7/6023b38f7691600647c3b20c/Shareable",
+  },
+  Store4: {
+    numberStatements: 233,
+    description:
+      "233 Statements | March-May 2020 | Overall attitude with customers and colleagues?, How is their work ethic?, Are they likely to pull in others to close sales?, EOG Resources, Johnson Controls, Sherwin-Williams, Con-way, Health Net, Lowe's, Kimberly-Clark",
+    authorizationHeader:
+      "Basic M2RlYTdkZjAxMjM0ODVlN2E2Yjk0MTFiOWE4YjM5NDY0ZDczYWE3OTowYmQ4Y2E4N2EzOWQ2NDllMjA5ODI0YzdlY2RmMDRhNWMxMzVhMjll",
+    endpointDashboard:
+      "http://192.168.1.69/dashboards/6023b6407691600647c3b216/6023b6d17691600647c3b22b/Shareable",
+  },
+};
---
+++ src/script/populateBlockchain.js
@@ -0,0 +1,88 @@
+const Web3 = require("web3");
+const ContractABI = require("../../build/contracts/LearningDataContract.json");
+const data = require("./data");
+
+function getWeb3() {
+  if (typeof web3 !== "undefined") {
+    let web3 = new Web3(web3.currentProvider);
+    console.log("Detected");
+    return web3;
+  } else {
+    let web3 = new Web3(
+      new Web3.providers.WebsocketProvider("ws://localhost:8545", {
+        clientOptions: {
+          maxReceivedFrameSize: 100000000,
+          maxReceivedMessageSize: 100000000,
+        },
+      })
+    );
+    console.log("8545");
+    return web3;
+  }
+}
+
+function closeWeb3(web3) {
+  web3.currentProvider.connection.close();
+}
+
+function fetchContract(web3) {
+  return new Promise(async (resolve, reject) => {
+    var netId = await web3.eth.net.getId();
+    var netData = ContractABI.networks[netId];
+    //console.log(netId);
+
+    if (netData) {
+      //console.log(netData.address);
+      var DataMarket = new web3.eth.Contract(ContractABI.abi, netData.address);
+      resolve(DataMarket);
+    } else {
+      //console.log("Contract has not been deployed to detected network");
+      reject("Contract has not been deployed to detected network");
+    }
+  });
+}
+
+async function populate(cb) {
+  const accountsToPay = [accounts[8], accounts[9]];
+
+  for (var i = 1; i <= 4; i++) {
+    const endpointAPI = data.endpointAPI;
+    const endpointDashboard = data[`Store${i}`].endpointDashboard;
+    const authorizationHeader = data[`Store${i}`].authorizationHeader;
+    const description = data[`Store${i}`].description;
+
+    const number = data[`Store${i}`].numberStatements / 100;
+    const price = web3.utils.toWei(number.toString(), "Ether");
+
+    await LearningDataContract.methods
+      .registerOffer(
+        endpointAPI,
+        endpointDashboard,
+        authorizationHeader,
+        description,
+        price,
+        accountsToPay
+      )
+      .send({ from: accounts[0], gas: 3000000 });
+  }
+
+  cb();
+}
+
+async function main() {
+  this.web3 = getWeb3();
+  this.LearningDataContract = await fetchContract(web3);
+  this.accounts = await web3.eth.getAccounts();
+
+  populate.bind(this)(async () => {
+    const numOffers = await LearningDataContract.methods.numOffers().call();
+    console.log(numOffers, "Offers:");
+
+    for (let i = 0; i < numOffers; i++) {
+      console.log(await LearningDataContract.methods.getOffer(i).call());
+    }
+    closeWeb3(web3);
+  });
+}
+
+main();
